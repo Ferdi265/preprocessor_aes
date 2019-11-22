4d916891907e3d5edc2dc275948100cfc1a7fd37
Ferdinand Bachmann <theferdi265@gmail.com>
2019-11-22 21:49:17 +0100

    implement keyschedule.h

--- aes.h
+++ aes.h
@@ -1,11 +1,21 @@
 #include "add_round_key.h"
+#define ROUND_CONSTANT 1
 #include "round.h"
+#define ROUND_CONSTANT 2
 #include "round.h"
+#define ROUND_CONSTANT 4
 #include "round.h"
+#define ROUND_CONSTANT 8
 #include "round.h"
+#define ROUND_CONSTANT 16
 #include "round.h"
+#define ROUND_CONSTANT 32
 #include "round.h"
+#define ROUND_CONSTANT 64
 #include "round.h"
+#define ROUND_CONSTANT 128
 #include "round.h"
+#define ROUND_CONSTANT 27
 #include "round.h"
+#define ROUND_CONSTANT 54
 #include "final_round.h"
--- keyschedule.h
+++ keyschedule.h
@@ -1,17 +1,34 @@
-#define K0_NEXT ???
-#define K1_NEXT ???
-#define K2_NEXT ???
-#define K3_NEXT ???
-#define K4_NEXT ???
-#define K5_NEXT ???
-#define K6_NEXT ???
-#define K7_NEXT ???
-#define K8_NEXT ???
-#define K9_NEXT ???
-#define K10_NEXT ???
-#define K11_NEXT ???
-#define K12_NEXT ???
-#define K13_NEXT ???
-#define K14_NEXT ???
-#define K15_NEXT ???
+#define SBOX_NEXT K13
+#include "literals/sbox.h"
+#define T0_NEXT (SBOX ^ ROUND_CONSTANT)
+#include "literals/T0.h"
+#define SBOX_NEXT K14
+#include "literals/sbox.h"
+#define T1_NEXT SBOX
+#include "literals/T1.h"
+#define SBOX_NEXT K15
+#include "literals/sbox.h"
+#define T2_NEXT SBOX
+#include "literals/T2.h"
+#define SBOX_NEXT K12
+#include "literals/sbox.h"
+#define T3_NEXT SBOX
+#include "literals/T3.h"
+
+#define K0_NEXT (K0 ^ T0)
+#define K1_NEXT (K1 ^ T1)
+#define K2_NEXT (K2 ^ T2)
+#define K3_NEXT (K3 ^ T3)
+#define K4_NEXT (K0_NEXT ^ K4)
+#define K5_NEXT (K1_NEXT ^ K5)
+#define K6_NEXT (K2_NEXT ^ K6)
+#define K7_NEXT (K3_NEXT ^ K7)
+#define K8_NEXT (K4_NEXT ^ K8)
+#define K9_NEXT (K5_NEXT ^ K9)
+#define K10_NEXT (K6_NEXT ^ K10)
+#define K11_NEXT (K7_NEXT ^ K11)
+#define K12_NEXT (K8_NEXT ^ K12)
+#define K13_NEXT (K9_NEXT ^ K13)
+#define K14_NEXT (K10_NEXT ^ K14)
+#define K15_NEXT (K11_NEXT ^ K15)
 #include "transform_key.h"
