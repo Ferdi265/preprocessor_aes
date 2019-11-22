8fcb0565821a342b43530e3c57b125403799d46e
Ferdinand Bachmann <theferdi265@gmail.com>
2019-11-22 21:52:18 +0100

    fix redefinition of ROUND_CONSTANT

--- aes.h
+++ aes.h
@@ -1,21 +1,30 @@
 #include "add_round_key.h"
 #define ROUND_CONSTANT 1
 #include "round.h"
+#undef ROUND_CONSTANT
 #define ROUND_CONSTANT 2
 #include "round.h"
+#undef ROUND_CONSTANT
 #define ROUND_CONSTANT 4
 #include "round.h"
+#undef ROUND_CONSTANT
 #define ROUND_CONSTANT 8
 #include "round.h"
+#undef ROUND_CONSTANT
 #define ROUND_CONSTANT 16
 #include "round.h"
+#undef ROUND_CONSTANT
 #define ROUND_CONSTANT 32
 #include "round.h"
+#undef ROUND_CONSTANT
 #define ROUND_CONSTANT 64
 #include "round.h"
+#undef ROUND_CONSTANT
 #define ROUND_CONSTANT 128
 #include "round.h"
+#undef ROUND_CONSTANT
 #define ROUND_CONSTANT 27
 #include "round.h"
+#undef ROUND_CONSTANT
 #define ROUND_CONSTANT 54
 #include "final_round.h"
